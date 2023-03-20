3afb2448773728d82e8e24565785964d175cfb40
Samuel Seakhela <samuelseakhela@gmail.com>
2023-03-20 04:09:48 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'numbers of base16'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int o;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (o = 97; o <= 102; n++)
+	{
+		putchar(o);
+	}
+	putchar('\n');
+	return (0);
+}
