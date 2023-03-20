628301a7ed30c1496c8bd2ef501b0d1e062c03de
Samuel Seakhela <samuelseakhela@gmail.com>
2023-03-20 05:18:56 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print lowercase and UPPERCASE alphabets'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int o;
+
+	for (n == 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	for (o == 65; o <= 90; n++)
+	{
+		putchar(o);
+	}
+	putchar('\n');
+	return (0);
+}
