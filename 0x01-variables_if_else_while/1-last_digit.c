5b86686f2d1c12065a0ed33aa091abebd081bdaf
Samuel Seakhela <samuelseakhela@gmail.com>
2023-03-20 05:58:30 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,10 @@ int main(void)
 	if (last_digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
-		printf("\n");
 	}
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0", n, last_digit);
 	}
 	else
 	{
