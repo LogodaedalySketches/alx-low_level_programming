dbb1b7558830e5164a028c265de3679993f958c3
Samuel Seakhela <samuelseakhela@gmail.com>
2023-03-22 02:58:38 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - lowercase characters
+ * @c: 'check for lower case c'
+ * Return: 1 for lowercase c and 0 for everything else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
