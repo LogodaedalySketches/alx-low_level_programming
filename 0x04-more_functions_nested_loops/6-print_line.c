5bc8637b747b298f6131a1bf4d308f49832ec441
Samuel Seakhela <samuelseakhela@gmail.com>
2023-04-20 07:04:40 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,12 +5,18 @@
  */
 void print_line(int n)
 {
-	for (n = 1; n >= 1; n++)
-	{
-		_putchar('_');
-	}
 	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
